56ed45dafa692f5abe6bab026922a1ef220b4827
Danil Lakhtin <danil.lakhtin@corp.badoo.com>
2020-06-27 17:07:27 +0300

    Test Field init

---
+++ CoreTests/FieldTests.swift
@@ -0,0 +1,19 @@
+//
+//  FieldTests.swift
+//  CoreTests
+//
+//  Created by Danil Lahtin on 27.06.2020.
+//  Copyright © 2020 Danil Lahtin. All rights reserved.
+//
+
+import XCTest
+
+final class Field {
+
+}
+
+final class FieldTests: XCTestCase {
+    func test_init() {
+        let field = Field()
+    }
+}
