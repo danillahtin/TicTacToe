d6bf4a37ec83b2456f11e7f25cd947479c080e9f
Danil Lakhtin <danil.lakhtin@corp.badoo.com>
2020-07-12 15:38:57 +0300

    Win coordinates

--- CoreTests/DefaultGameRulesTests.swift
+++ CoreTests/DefaultGameRulesTests.swift
@@ -12,27 +12,66 @@ import Core
 private typealias Field = Core.Field<Player>
 
 final class DefaultGameRules {
-    let size = 3
     let field: Core.Field<Player>
+    let winCoordinates: [[Core.Field<Player>.Coordinate]]
 
-    init(field: Core.Field<Player>) {
+    init(
+        field: Core.Field<Player>,
+        winCoordinates: [[Core.Field<Player>.Coordinate]])
+    {
         self.field = field
+        self.winCoordinates = winCoordinates
+    }
+
+    static var winCoordinates: [[Core.Field<Player>.Coordinate]] {
+        [
+            [
+                Field.Coordinate(x: 0, y: 0),
+                Field.Coordinate(x: 0, y: 1),
+                Field.Coordinate(x: 0, y: 2),
+            ],
+            [
+                Field.Coordinate(x: 1, y: 0),
+                Field.Coordinate(x: 1, y: 1),
+                Field.Coordinate(x: 1, y: 2),
+            ],
+            [
+                Field.Coordinate(x: 2, y: 0),
+                Field.Coordinate(x: 2, y: 1),
+                Field.Coordinate(x: 2, y: 2),
+            ],
+            [
+                Field.Coordinate(x: 0, y: 0),
+                Field.Coordinate(x: 1, y: 0),
+                Field.Coordinate(x: 2, y: 0),
+            ],
+            [
+                Field.Coordinate(x: 0, y: 1),
+                Field.Coordinate(x: 1, y: 1),
+                Field.Coordinate(x: 2, y: 1),
+            ],
+            [
+                Field.Coordinate(x: 0, y: 2),
+                Field.Coordinate(x: 1, y: 2),
+                Field.Coordinate(x: 2, y: 2),
+            ],
+            [
+                Field.Coordinate(x: 0, y: 0),
+                Field.Coordinate(x: 1, y: 1),
+                Field.Coordinate(x: 2, y: 2),
+            ],
+            [
+                Field.Coordinate(x: 2, y: 0),
+                Field.Coordinate(x: 1, y: 1),
+                Field.Coordinate(x: 0, y: 2),
+            ],
+        ]
     }
 
     func getWinner() -> Player? {
-        var winCoordinates: [[Field.Coordinate]] = (0..<size).flatMap({ x in
-            [
-                (0..<size).map({ .init(x: x, y: $0) }),
-                (0..<size).map({ .init(x: $0, y: x) }),
-            ]
-        })
-
-        winCoordinates.append((0..<size).map({ .init(x: $0, y: $0) }))
-        winCoordinates.append((0..<size).map({ .init(x: $0, y: size - $0 - 1) }))
-
-        return winCoordinates.first(where: filledWithSamePlayer).flatMap({
-            try! field.value(at: $0.first!)
-        })
+        winCoordinates
+            .first(where: filledWithSamePlayer)
+            .flatMap({ try! field.value(at: $0.first!) })
     }
 
     private func filledWithSamePlayer(coordinates: [Field.Coordinate]) -> Bool {
@@ -47,139 +86,79 @@ final class DefaultGameRules {
 }
 
 final class DefaultGameRulesTests: XCTestCase {
+    private let fieldSize = 1
+    private let winCoordinates: [[Field.Coordinate]] = [[.init(x: 0, y: 0)]]
+
     func test_init() {
         let _ = makeSut()
     }
 
     func test_getWinner_returnsWinner() {
         assert(winner: nil) { _ in }
-
         assert(winner: .cross) {
             try! $0.put(.cross, at: .init(x: 0, y: 0))
-            try! $0.put(.cross, at: .init(x: 0, y: 1))
-            try! $0.put(.cross, at: .init(x: 0, y: 2))
-        }
-
-        assert(winner: .cross) {
-            try! $0.put(.cross, at: .init(x: 1, y: 0))
-            try! $0.put(.cross, at: .init(x: 1, y: 1))
-            try! $0.put(.cross, at: .init(x: 1, y: 2))
-        }
-
-        assert(winner: .cross) {
-            try! $0.put(.cross, at: .init(x: 2, y: 0))
-            try! $0.put(.cross, at: .init(x: 2, y: 1))
-            try! $0.put(.cross, at: .init(x: 2, y: 2))
         }
 
         assert(winner: .zero) {
             try! $0.put(.zero, at: .init(x: 0, y: 0))
-            try! $0.put(.zero, at: .init(x: 0, y: 1))
-            try! $0.put(.zero, at: .init(x: 0, y: 2))
         }
+    }
 
-        assert(winner: .zero) {
-            try! $0.put(.zero, at: .init(x: 1, y: 0))
-            try! $0.put(.zero, at: .init(x: 1, y: 1))
-            try! $0.put(.zero, at: .init(x: 1, y: 2))
-        }
-
-        assert(winner: .zero) {
-            try! $0.put(.zero, at: .init(x: 2, y: 0))
-            try! $0.put(.zero, at: .init(x: 2, y: 1))
-            try! $0.put(.zero, at: .init(x: 2, y: 2))
-        }
-
-        // --
-
-        assert(winner: .cross) {
-            try! $0.put(.cross, at: .init(x: 0, y: 0))
-            try! $0.put(.cross, at: .init(x: 1, y: 0))
-            try! $0.put(.cross, at: .init(x: 2, y: 0))
-        }
-
-        assert(winner: .cross) {
-            try! $0.put(.cross, at: .init(x: 0, y: 1))
-            try! $0.put(.cross, at: .init(x: 1, y: 1))
-            try! $0.put(.cross, at: .init(x: 2, y: 1))
-        }
-
-        assert(winner: .cross) {
-            try! $0.put(.cross, at: .init(x: 0, y: 2))
-            try! $0.put(.cross, at: .init(x: 1, y: 2))
-            try! $0.put(.cross, at: .init(x: 2, y: 2))
-        }
-
-        assert(winner: .zero) {
-            try! $0.put(.zero, at: .init(x: 0, y: 0))
-            try! $0.put(.zero, at: .init(x: 1, y: 0))
-            try! $0.put(.zero, at: .init(x: 2, y: 0))
-        }
-
-        assert(winner: .zero) {
-            try! $0.put(.zero, at: .init(x: 0, y: 1))
-            try! $0.put(.zero, at: .init(x: 1, y: 1))
-            try! $0.put(.zero, at: .init(x: 2, y: 1))
-        }
-
-        assert(winner: .zero) {
-            try! $0.put(.zero, at: .init(x: 0, y: 2))
-            try! $0.put(.zero, at: .init(x: 1, y: 2))
-            try! $0.put(.zero, at: .init(x: 2, y: 2))
-        }
-
-        // --
-
-        assert(winner: .cross) {
-            try! $0.put(.cross, at: .init(x: 0, y: 0))
-            try! $0.put(.cross, at: .init(x: 1, y: 1))
-            try! $0.put(.cross, at: .init(x: 2, y: 2))
-        }
-
-        assert(winner: .cross) {
-            try! $0.put(.cross, at: .init(x: 0, y: 2))
-            try! $0.put(.cross, at: .init(x: 1, y: 1))
-            try! $0.put(.cross, at: .init(x: 2, y: 0))
-        }
-
-        assert(winner: .zero) {
-            try! $0.put(.zero, at: .init(x: 0, y: 0))
-            try! $0.put(.zero, at: .init(x: 1, y: 1))
-            try! $0.put(.zero, at: .init(x: 2, y: 2))
-        }
-
-        assert(winner: .zero) {
-            try! $0.put(.zero, at: .init(x: 0, y: 2))
-            try! $0.put(.zero, at: .init(x: 1, y: 1))
-            try! $0.put(.zero, at: .init(x: 2, y: 0))
-        }
-
-        // --
-        assert(winner: nil) {
-            try! $0.put(.zero, at: .init(x: 0, y: 1))
-            try! $0.put(.zero, at: .init(x: 1, y: 1))
-            try! $0.put(.zero, at: .init(x: 2, y: 0))
-        }
-
-        assert(winner: nil) {
-            try! $0.put(.cross, at: .init(x: 0, y: 1))
-            try! $0.put(.cross, at: .init(x: 1, y: 1))
-        }
-
-        assert(winner: nil) {
-            try! $0.put(.cross, at: .init(x: 0, y: 1))
-            try! $0.put(.zero, at: .init(x: 2, y: 1))
-        }
+    func test_winCoordinates() {
+        XCTAssertEqual(DefaultGameRules.winCoordinates, [
+            [
+                Field.Coordinate(x: 0, y: 0),
+                Field.Coordinate(x: 0, y: 1),
+                Field.Coordinate(x: 0, y: 2),
+            ],
+            [
+                Field.Coordinate(x: 1, y: 0),
+                Field.Coordinate(x: 1, y: 1),
+                Field.Coordinate(x: 1, y: 2),
+            ],
+            [
+                Field.Coordinate(x: 2, y: 0),
+                Field.Coordinate(x: 2, y: 1),
+                Field.Coordinate(x: 2, y: 2),
+            ],
+            [
+                Field.Coordinate(x: 0, y: 0),
+                Field.Coordinate(x: 1, y: 0),
+                Field.Coordinate(x: 2, y: 0),
+            ],
+            [
+                Field.Coordinate(x: 0, y: 1),
+                Field.Coordinate(x: 1, y: 1),
+                Field.Coordinate(x: 2, y: 1),
+            ],
+            [
+                Field.Coordinate(x: 0, y: 2),
+                Field.Coordinate(x: 1, y: 2),
+                Field.Coordinate(x: 2, y: 2),
+            ],
+            [
+                Field.Coordinate(x: 0, y: 0),
+                Field.Coordinate(x: 1, y: 1),
+                Field.Coordinate(x: 2, y: 2),
+            ],
+            [
+                Field.Coordinate(x: 2, y: 0),
+                Field.Coordinate(x: 1, y: 1),
+                Field.Coordinate(x: 0, y: 2),
+            ],
+        ])
     }
 
     // MARK: - Helpers
 
     private func makeSut(field: Field? = nil) -> DefaultGameRules {
-        DefaultGameRules(field: field ?? makeField())
+        DefaultGameRules(
+            field: field ?? makeField(),
+            winCoordinates: winCoordinates)
     }
 
     private func makeField() -> Field {
-        Field(size: 3)!
+        Field(size: fieldSize)!
     }
     private func assert(
         winner: Player?,
